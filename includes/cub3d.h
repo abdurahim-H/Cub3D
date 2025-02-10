69bd71419121627d8d8116a28d6e5df6c37054a6
abdurahim-H <abul.hudul@gmail.com>
2025-02-10 04:34:10 +0100

    updated cub3d.h

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,13 +1,18 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include <mlx.h>
-#include <stdio.h>
-#include <stdlib.h>
+# include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 int		ft_strcmp(const char *s1, const char *s2);
 int		has_cub_extension(char *filename);
 int		is_save(char *s_argument);
-int check_arg(int argc, char **argv);
+int		check_arg(int argc, char **argv);
 
 #endif
