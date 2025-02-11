34ad6654caa9ed975d8213b7720f15b0ab69401f
abdurahim-H <abul.hudul@gmail.com>
2025-02-11 22:08:56 +0100

    get_next_line is completed and tested succesfully

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "cub3d.h"
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if (check_arg(argc, argv) == 0)
+	int			fd;
+	char		*line;
+
+	if (check_arg(argc, argv) != 0)
+		return (1);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
 	{
-		printf("Great Job!\n");
+		perror("Error opening file");
+		return (1);
 	}
+
+	while ((line = ft_getline(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+
+	close(fd);
 	return (0);
 }
