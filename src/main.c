f13e52ea417c5fcb1171f87f1fcccbe8bac197e2
abdurahim-H <abul.hudul@gmail.com>
2025-02-15 18:45:08 +0100

    okay i was being dramam queen, everything is good

--- src/main.c
+++ src/main.c
@@ -4,45 +4,15 @@
 #include <unistd.h>
 #include "cub3d.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	int			fd;
-// 	t_config	config;
-// 	int			map_count;
-// 	char		**map;
-
-// 	fd = 0;
-// 	map_count = 0;
-// 	map = NULL;
-// 	if (check_arg(argc, argv) != 0)
-// 		return (1);
-// 	fd = open(argv[1], O_RDONLY);
-// 	if (fd < 0)
-// 	{
-// 		perror("Error opening file");
-// 		return (1);
-// 	}
-// 	init_config(&config);
-// 	process_config(fd, &config);
-// 	map = collect_map_lines_rest(fd, &map_count);
-// 	config.map = map;
-// 	print_map(config.map, map_count);
-// 	calculate_map_dimensions(map, map_count);
-// 	close(fd);
-// 	return (0);
-// }
-
-
 int	main(int argc, char **argv)
 {
 	int			fd;
 	t_config	config;
-	int			map_count;
 
 	fd = 0;
-	map_count = 0;
 	if (check_arg(argc, argv) != 0)
 		return (1);
+
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
@@ -51,12 +21,13 @@ int	main(int argc, char **argv)
 	}
 	init_config(&config);
 	process_config(fd, &config);
+
 	if (!config.map)
-		config.map = collect_map_lines_rest(fd, &map_count);
-	else
-		map_count = 0;
-	print_map(config.map, map_count);
-	calculate_map_dimensions(config.map, map_count);
+		config.map = collect_map_lines_rest(fd, &config.map_count);
+
+	print_map(config.map, config.map_count);
+	calculate_map_dimensions(config.map, config.map_count);
+
 	close(fd);
 	return (0);
 }
--- src/map_utils_1.c
+++ src/map_utils_1.c
@@ -1,23 +1,23 @@
 #include "cub3d.h"
 
-char	**collect_map_with_first_line(
-	int fd, int *map_count, char *first_line)
+char	**collect_map_with_first_line(int fd, char *first_line,
+		int *final_count)
 {
 	char	**temp_map;
 	int		count;
-	int		ret;
+	int		newcount;
 
 	temp_map = NULL;
 	count = 0;
-	ret = append_line_to_map(&temp_map, count, first_line);
-	if (ret < 0)
+	newcount = append_line_to_map(&temp_map, count, first_line);
+	if (newcount < 0)
 	{
 		free(first_line);
 		return (NULL);
 	}
-	count = ret;
+	count = newcount;
 	temp_map = collect_map_lines_rest(fd, &count);
-	*map_count = count;
+	*final_count = count;
 	return (temp_map);
 }
 
@@ -28,11 +28,11 @@ void	process_config(int fd, t_config *cfg)
 	while (1)
 	{
 		line = ft_getline(fd);
-		if (line == NULL)
+		if (!line)
 			break ;
 		if (is_map_line(line))
 		{
-			cfg->map = collect_map_with_first_line(fd, &((int){0}), line);
+			cfg->map = collect_map_with_first_line(fd, line, &cfg->map_count);
 			break ;
 		}
 		parse_line(line, cfg);
@@ -58,21 +58,3 @@ void	calculate_map_dimensions(char **map, int map_count)
 	printf("Map dimensions: height = %d, width = %d\n",
 		map_count, max_width);
 }
-
-// void	calculate_map_dimensions(char **map, int map_count)
-// {
-// 	int	i;
-// 	int	max_width;
-// 	int	current_width;
-
-// 	i = 0;
-// 	max_width = 0;
-// 	while (i < map_count)
-// 	{
-// 		current_width = ft_strlen(map[i]);
-// 		if (current_width > max_width)
-// 			max_width = current_width;
-// 		i++;
-// 	}
-// 	printf("Map dimensions: height = %d, width = %d\n", map_count, max_width);
-// }
