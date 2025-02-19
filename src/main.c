1a919741a5d5ac77ee153e8ab8089b80a401b743
abdurahim-H <abul.hudul@gmail.com>
2025-02-19 08:50:41 +0100

    test set-up for minilibx

--- src/main.c
+++ src/main.c
@@ -1,8 +1,37 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "cub3d.h"
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	initialize_mlx(t_config config)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, config.width, config.height, "Hello world!");
+	img.img = mlx_new_image(mlx, config.width, config.height);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+		&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
 
 int	main(int argc, char **argv)
 {
@@ -27,5 +56,6 @@ int	main(int argc, char **argv)
 	print_map(config.map, config.map_count);
 	calculate_map_dimensions(config.map, config.map_count);
 	close(fd);
+	initialize_mlx(config);
 	return (0);
 }
